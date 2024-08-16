ddfa3d7419c1f81888b98a647df0bcb3c218b611
Dominik Rathmer <dominik.rathmer@asgoodasnew.com>
2024-08-16 13:32:31 +0200

    feat(PlayerReviveCompatEvent): Add knockdown compat

--- src/main/java/net/hammerclock/mmnmrevive/events/PlayerReviveCompatEvent.java
+++ src/main/java/net/hammerclock/mmnmrevive/events/PlayerReviveCompatEvent.java
@@ -11,22 +11,27 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.hammerclock.mmnmrevive.PlayerReviveCompatMod;
-
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particles.ParticleTypes;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-
+import xyz.pixelatedw.mineminenomi.abilities.KnockdownAbility;
 import xyz.pixelatedw.mineminenomi.api.helpers.AbilityHelper;
 import xyz.pixelatedw.mineminenomi.api.helpers.SoulboundItemHelper;
+import xyz.pixelatedw.mineminenomi.data.entity.ability.AbilityDataCapability;
+import xyz.pixelatedw.mineminenomi.data.entity.ability.IAbilityData;
 import xyz.pixelatedw.mineminenomi.data.entity.entitystats.EntityStatsCapability;
 import xyz.pixelatedw.mineminenomi.data.entity.entitystats.IEntityStats;
+import xyz.pixelatedw.mineminenomi.init.ModAbilityKeys;
+import xyz.pixelatedw.mineminenomi.init.ModEffects;
 import xyz.pixelatedw.mineminenomi.init.ModItems;
 import xyz.pixelatedw.mineminenomi.wypi.WyHelper;
 
@@ -37,20 +42,51 @@ public class PlayerReviveCompatEvent {
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public void onPlayerDeathEvent(LivingDeathEvent event) {
 		if (event.getEntity() instanceof ServerPlayerEntity) {
-			ServerPlayerEntity player = (ServerPlayerEntity) event.getEntity();
-			LOGGER.debug("Player {} has died and is bleeding out", player.getDisplayName().getString());
+			ServerPlayerEntity living = (ServerPlayerEntity) event.getEntityLiving();
+			DamageSource source = event.getSource();
+			Entity directEntity = source.getDirectEntity();
+			Entity trueEntity = source.getEntity();
 
-			IEntityStats deadPlayerEntityStats = EntityStatsCapability.get(player);
-			DamageSource deathCause = event.getSource();
-			LOGGER.debug("Source of death was {}", event.getSource().toString());
-			LOGGER.debug("Logging entity of deathcause of player: {}", deathCause.getEntity());
+			LivingEntity attacker = null;
+			
+			if (directEntity instanceof LivingEntity) {
+				attacker = (LivingEntity) directEntity;
+			}
+			else if (trueEntity instanceof LivingEntity) {
+				attacker = (LivingEntity) trueEntity;
+			}
+			LOGGER.debug("Player {} has died and is bleeding out", living.getDisplayName().getString());
 
-			if (deathCause.getMsgId().equals("heart_damage")) {
-				PlayerReviveServer.kill(player);
+			IEntityStats deadPlayerEntityStats = EntityStatsCapability.get(living);
+			LOGGER.debug("Source of death was {}", source);
+			LOGGER.debug("Logging entity of deathcause of player: {}", source.getEntity());
+
+			if (attacker != null) {
+			IAbilityData props = AbilityDataCapability.get(attacker);
+
+			boolean isKnockdownActive = false;
+				if (props != null) {
+					KnockdownAbility abl = props.getPassiveAbility(KnockdownAbility.INSTANCE);
+					if (abl != null) {
+						isKnockdownActive = !abl.getComponent(ModAbilityKeys.PAUSE_TICK).get().isPaused();
+					}
+				}
+				
+				if (isKnockdownActive) {
+					living.addEffect(new EffectInstance(ModEffects.UNCONSCIOUS.get(), 1800, 1));
+					living.setHealth(2.0f);
+					living.clearFire();
+					event.setCanceled(true);
+					return;
+				}
 			}
 
-			if (deathCause.getEntity() instanceof ServerPlayerEntity && !deadPlayerEntityStats.hasStrawDoll()) {
-				ServerPlayerEntity deathCausePlayer = (ServerPlayerEntity) deathCause.getEntity();
+			if (source.getMsgId().equals("heart_damage")) {
+				PlayerReviveServer.kill(living);
+			}
+
+			if (attacker instanceof ServerPlayerEntity && !deadPlayerEntityStats.hasStrawDoll()) {
+				ServerPlayerEntity deathCausePlayer = (ServerPlayerEntity) attacker.getEntity();
 				LOGGER.debug("Got the following Player as the death cause {}",
 						deathCausePlayer.getDisplayName().getString());
 				for (int i = 0; i < deathCausePlayer.inventory.items.size(); i++) {
@@ -66,12 +102,12 @@ public class PlayerReviveCompatEvent {
 							continue;
 						}
 
-						if (strawDollOwner.getValue() != player) {
+						if (strawDollOwner.getValue() != living) {
 							LOGGER.debug("Value is not a player but {}", strawDollOwner.getValue());
 							continue;
 						}
 
-						if (strawDollOwner.getValue() == player) {
+						if (strawDollOwner.getValue() == living) {
 							LOGGER.debug("Strawdoll is soulbound to player!");
 							this.spawnParticles((ServerWorld) deathCausePlayer.level, deathCausePlayer.getX(),
 									deathCausePlayer.getY(), deathCausePlayer.getZ());
